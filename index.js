1b12da343ec43fb59985d4b7eac31a3d652395f8
Semyon Rozhkov <semro@outlook.com>
2025-09-23 00:20:53 +0300

    module-konva-10

---
+++ index.js
@@ -0,0 +1,31 @@
+// const Konva = require("konva");
+
+import Konva from "konva";
+import "konva/skia-backend"; // or 'konva/skia-backend'
+
+// Create a stage
+const stage = new Konva.Stage({
+  container: "container", // This will be ignored in Node.js
+  width: 800,
+  height: 600,
+});
+
+const layer = new Konva.Layer();
+stage.add(layer);
+
+const rect = new Konva.Rect({
+  x: 50,
+  y: 50,
+  width: 100,
+  height: 100,
+  fill: "black",
+  draggable: true,
+});
+
+layer.add(rect).draw();
+
+// ... the rest of your konva code
+
+// Export as data URL
+const dataURL = stage.toDataURL();
+console.log(dataURL);
